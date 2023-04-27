6e0cf2a132bb491aede9415fbf7ea41ffd381ba2
Tatiana Yuryeva <yuryevat@list.ru>
2023-04-27 20:34:51 +0200

    ESlint installed

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
-const goblin = document.createElement('img')
-goblin.setAttribute('src', '/src/img/goblin.png')
+const goblin = document.createElement("img");
+goblin.setAttribute("src", "/src/img/goblin.png");
 
-const getHole = index => document.querySelector(`#hole${index}`)
+const getHole = (index) => document.querySelector(`#hole${index}`);
 
-const activateHole = index => getHole( index ).append(goblin)
+const activateHole = (index) => getHole(index).append(goblin);
 
 const holeInterval = setInterval(() => {
-  const activeHoleIndex = Math.floor( 1 + Math.random() * 16 );
-  activateHole(activeHoleIndex)
-}, 1500)
+  const activeHoleIndex = Math.floor(1 + Math.random() * 16);
+  activateHole(activeHoleIndex);
+}, 1500);
